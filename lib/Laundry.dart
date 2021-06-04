450cee44549409c6e2b03d05cf2d7295486c8513
Akirakii <akiraki.awa@gmail.com>
2021-06-04 11:22:27 -0500

    list laundries implemented

---
+++ lib/Laundry.dart
@@ -0,0 +1,18 @@
+class Laundry {
+  String name;
+  String address;
+  double price;
+  String email;
+  String district;
+  int phone;
+  String imageLogo;
+
+  Laundry(
+      {required this.name,
+      required this.address,
+      required this.price,
+      required this.email,
+      required this.district,
+      required this.phone,
+      required this.imageLogo});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,144 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:washi_mobile_app_flutter/Laundry.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Name generator...",
+      home: RandomWords(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class RandomWords extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  RandomWordsState createState() => RandomWordsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class RandomWordsState extends State<RandomWords> {
+  final List<Laundry> laundries = <Laundry>[
+    Laundry(
+        name: "Lavanderia Akiraki",
+        address: "Av. La marina",
+        price: 9.99,
+        email: "akira@gmail.com",
+        district: "San Miguel",
+        phone: 958158852,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Lavanderia Cledmir",
+        address: "Av. faucett",
+        price: 10,
+        email: "cledmir@gmail.com",
+        district: "San Miguel",
+        phone: 985654123,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Aqui lavamos bien",
+        address: "Av. Universitaria",
+        price: 9.99,
+        email: "aquilavamos@gmail.com",
+        district: "Cercado de Lima",
+        phone: 990587526,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Lavanderia Akiraki",
+        address: "Av. La marina",
+        price: 9.99,
+        email: "akira@gmail.com",
+        district: "San Miguel",
+        phone: 958158852,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Lavanderia Cledmir",
+        address: "Av. faucett",
+        price: 10,
+        email: "cledmir@gmail.com",
+        district: "San Miguel",
+        phone: 985654123,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Aqui lavamos bien",
+        address: "Av. Universitaria",
+        price: 9.99,
+        email: "aquilavamos@gmail.com",
+        district: "Cercado de Lima",
+        phone: 990587526,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Lavanderia Akiraki",
+        address: "Av. La marina",
+        price: 9.99,
+        email: "akira@gmail.com",
+        district: "San Miguel",
+        phone: 958158852,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Lavanderia Cledmir",
+        address: "Av. faucett",
+        price: 10,
+        email: "cledmir@gmail.com",
+        district: "San Miguel",
+        phone: 985654123,
+        imageLogo: "sample"),
+    Laundry(
+        name: "Aqui lavamos bien",
+        address: "Av. Universitaria",
+        price: 9.99,
+        email: "aquilavamos@gmail.com",
+        district: "Cercado de Lima",
+        phone: 990587526,
+        imageLogo: "sample"),
+  ];
+  final TextStyle biggerFont = TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Name generator..."),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: buildLaundries(),
+    );
+  }
+
+  Widget buildLaundries() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16),
+      itemCount: laundries.length,
+      itemBuilder: (BuildContext context, int i) {
+        return buildLaundryCard(laundries[i]);
+      },
+    );
+  }
+
+  Widget buildLaundryCard(Laundry laundry) {
+    return Center(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: FlutterLogo(size: 56.0),
+                title: Text(laundry.name),
+                subtitle: Text(
+                    "${laundry.address}\n${laundry.district}\n${laundry.email}\n${laundry.phone}"),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
