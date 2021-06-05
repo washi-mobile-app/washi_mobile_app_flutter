71eb6d247d20c130939f12353d4e79711f60dc0d
cledmir <56932765+cledmir@users.noreply.github.com>
2021-06-04 21:06:42 -0500

    uppdate

--- lib/laundryDetail.dart
+++ lib/laundryDetail.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:washi_mobile_app_flutter/entities/laundry.dart';
 
-
 class LaundryDetailScreen extends StatelessWidget{
   final Laundry laundry;
   const LaundryDetailScreen({Key? key, required this.laundry}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    String dropdownValue = 'One';
+    return Scaffold(
+      body: MyStatefulWidget(laundry: laundry),
+    );
+  }
+
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  final Laundry laundry;
+  const MyStatefulWidget({Key? key, required this.laundry}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _LaundryDetailScreen(laundry);
+}
+class _LaundryDetailScreen extends State<MyStatefulWidget>{
+  final Laundry laundry;
+
+  _LaundryDetailScreen(this.laundry);
+  String service = 'Lavado al agua';
+  String material = 'lana';
+  String detergent = 'ariel';
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(laundry.name)
@@ -17,44 +38,120 @@ class LaundryDetailScreen extends StatelessWidget{
 
           children: <Widget>[
             Expanded(
-                child: FittedBox(
-                  fit: BoxFit.cover,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
                   child: Image.network(
                     laundry.imageLogo,
-                    fit: BoxFit.cover,
+                    width: 150,
+                    height: 150,
                   ),
                 )
             ),
             Expanded(
-              child: Text('${laundry.district} \n ${laundry.phone} \n ${laundry.email}'),
+                child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text('${laundry.district} \n \n ${laundry.phone} \n \n ${laundry.email}')
+                )
             )
           ],
         ),
         Row(
           children: <Widget>[
             Expanded(
-                child: DropdownButton<String>(
-                  value: dropdownValue,
-                  icon: const
-                  Icon(Icons.arrow_downward_sharp),
-                  iconSize: 24,
-                  elevation: 16,
-                  items: <String>['Lavanderia', 'Planchado', 'Secado']
-                      .map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (_) {},
-
+                child: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: DropdownButton<String>(
+                    value: service,
+                    icon: const
+                    Icon(Icons.arrow_downward_sharp),
+                    iconSize: 24,
+                    elevation: 16,
+                    items: <String>['Lavado al agua', 'LAvado al seco', 'Lavado a mano', 'planchado']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        service = newValue!;
+                      });
+                    },
+                  )
                 )
             ),
             Expanded(
-              child: Text('${laundry.district} \n ${laundry.phone} \n ${laundry.email}'),
-            )
+                child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: DropdownButton<String>(
+                      value: material,
+                      icon: const
+                      Icon(Icons.arrow_downward_sharp),
+                      iconSize: 24,
+                      elevation: 16,
+                      items: <String>['lana', 'algodon', 'lino']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (_) {},
+                    )
+                )
+            ),
+            Expanded(
+                child: Padding(
+                    padding: EdgeInsets.all(16),
+                    child: DropdownButton<String>(
+                      value: detergent,
+                      icon: const
+                      Icon(Icons.arrow_downward_sharp),
+                      iconSize: 24,
+                      elevation: 16,
+                      items: <String>['ariel', 'ace', 'skip']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+
+                      },
+                    )
+                )
+            ),
+
           ],
         ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child:  Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Tipo de servicio:  $service \n \n Kilogramos: 1 \n \n Precio: s/.12.00',
+                    textAlign: TextAlign.center,
+                  )
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child:  Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'Tipo de servicio:  $service \n \n Kilogramos: 1 \n \n Precio: s/.12.00',
+                    textAlign: TextAlign.center,
+                  )
+              ),
+            ),
+          ],
+        )
       ],
       )
 
