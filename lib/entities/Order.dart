a495045e1283424275d4286138d40ea5707e37d8
Lucero Gejaño <anlugeri2001@gmail.com>
2021-06-04 20:13:52 -0500

    Update Entity Order

---
+++ lib/entities/Order.dart
@@ -0,0 +1,16 @@
+class Order {
+  String laundryName;
+  String cost;
+  String status;
+  String date;
+  String deliveryDate;
+
+  Order(
+  {
+    required this.laundryName,
+    required this.cost,
+    required this.status,
+    required this.date,
+    required this.deliveryDate
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'entities/Order.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -34,13 +36,34 @@ class OrdersList extends StatefulWidget {
 }
 
 class ListItemWidget extends State<OrdersList> {
-  List items = getOrdersList();
+  final List<Order> orders = <Order>[
+    Order(
+      laundryName: "Lavandería Don Pepe",
+      cost: "S/. 44.60",
+      status: "En camino",
+      date: "05/05/21",
+      deliveryDate: "08/05/21"),
+    Order(
+        laundryName: "Lavandería Don Pepe",
+        cost: "S/. 30.50",
+        status: "En camino",
+        date: "06/06/21",
+        deliveryDate: "08/06/21"),
+    Order(
+        laundryName: "Lavandería Don Pepe",
+        cost: "S/. 35.00",
+        status: "Entregado",
+        date: "28/04/21",
+        deliveryDate: "02/05/21"),
+  ];
+
   final TextStyle biggerFont = TextStyle(fontSize: 20);
+  
   @override
   Widget build(BuildContext context) {
     return Container(
         child: ListView.builder(
-          itemCount: items.length,
+          itemCount: orders.length,
           itemBuilder: (context, index) {
             return new Container(
               child: Card(
@@ -72,13 +95,13 @@ class ListItemWidget extends State<OrdersList> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  items[index],
+                                  orders[index].laundryName,
                                   style: biggerFont,
                                 ),
                                 Padding(
                                   padding: EdgeInsets.fromLTRB(0, 10, 0, 3),
                                   child: Container(
-                                    child: Text("S/. 44.60",textAlign: TextAlign.center, style: TextStyle(
+                                    child: Text(orders[index].cost,textAlign: TextAlign.center, style: TextStyle(
                                         fontSize: 18
                                     ),),
                                   ),
@@ -86,7 +109,7 @@ class ListItemWidget extends State<OrdersList> {
                                 Padding(
                                   padding: EdgeInsets.fromLTRB(0, 5, 0, 3),
                                   child: Container(
-                                    child: Text("En camino",style: TextStyle(
+                                    child: Text(orders[index].status,style: TextStyle(
                                       fontSize: 19,
                                       color: Colors.blueAccent,
                                     ),),
@@ -99,18 +122,7 @@ class ListItemWidget extends State<OrdersList> {
                       ],
                     ),
                   ),
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute<void>(
-                            builder: (BuildContext context) {
-                              return Scaffold(
-                                appBar: AppBar(
-                                  title: Text("Detalle del pedido"),
-                                ),
-                                body: Center(child: OrderDetail()));
-                            })
-                    );
-                  },
+                  onTap: () => details(orders[index])
                 ),
               ),
             );
@@ -118,176 +130,171 @@ class ListItemWidget extends State<OrdersList> {
         ));
   }
 
-  static List getOrdersList() {
-    List list = List.generate(3, (i) {
-      return "Lavandería Don Pepe";
-    });
-    return list;
-  }
-}
-class OrderDetail extends StatefulWidget {
-  @override
-  OrderDetailState createState() => OrderDetailState();
-}
-
-class OrderDetailState extends State<OrderDetail> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 15),
-              child: Text(
-                "Lavandería Don Pepe", style: TextStyle(fontSize: 25),
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
-                    child: Text(
-                      "Fecha", style: TextStyle(fontSize: 18),
-                    ),
+  void details(Order order) {
+    Navigator.of(context).push(
+        MaterialPageRoute<void>(
+            builder: (BuildContext context) {
+              return Scaffold(
+                  appBar: AppBar(
+                    title: Text("Detalle del pedido"),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(160, 10, 0, 15),
-                    child: Text(
-                      "05/05/21", style: TextStyle(fontSize: 18),
+                  body: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 20, 0, 15),
+                          child: Text(
+                          order.laundryName, style: TextStyle(fontSize: 25),
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
+                                child: Text(
+                                  "Fecha", style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(160, 10, 0, 15),
+                                child: Text(
+                                  order.date, style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
+                                child: Text(
+                                  "Fecha de entrega:", style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(65, 10, 0, 15),
+                                child: Text(
+                                  order.deliveryDate, style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
+                                child: Text(
+                                  "Estado:", style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(150, 10, 0, 15),
+                                child: Text(
+                                  order.status, style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
+                                child: Text(
+                                  "Lavado al seco de lana x 1kg", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
+                                child: Text(
+                                  "S/. 12.00", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
+                                child: Text(
+                                  "Lavado en agua de algodón x 2kg", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(27, 10, 0, 15),
+                                child: Text(
+                                  "S/. 17.00", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
+                                child: Text(
+                                  "Lavado de zapatillas de tela x 1par", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(20, 10, 0, 15),
+                                child: Text(
+                                  "S/. 10.00", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
+                                child: Text(
+                                  "Delivery", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(210, 10, 0, 15),
+                                child: Text(
+                                  "S/. 05.00", style: TextStyle(fontSize: 16),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
+                                child: Text(
+                                  "Total", style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(170, 10, 0, 15),
+                                child: Text(
+                                  order.cost, style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
-                    child: Text(
-                      "Fecha de entrega:", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(65, 10, 0, 15),
-                    child: Text(
-                      "08/05/21", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
-                    child: Text(
-                      "Estado:", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(150, 10, 0, 15),
-                    child: Text(
-                      "Entregado", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
-                    child: Text(
-                      "Lavado al seco de lana x 1kg", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
-                    child: Text(
-                      "S/. 12.00", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
-                    child: Text(
-                      "Lavado en agua de algodón x 2kg", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(27, 10, 0, 15),
-                    child: Text(
-                      "S/. 17.00", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
-                    child: Text(
-                      "Lavado de zapatillas de tela x 1par", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 0, 15),
-                    child: Text(
-                      "S/. 10.00", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(40, 10, 0, 15),
-                    child: Text(
-                      "Delivery", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(210, 10, 0, 15),
-                    child: Text(
-                      "S/. 05.00", style: TextStyle(fontSize: 16),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(60, 10, 0, 15),
-                    child: Text(
-                      "Total", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(170, 10, 0, 15),
-                    child: Text(
-                      "S/. 44.60", style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-      ),
+                  ));
+            })
     );
   }
 }
