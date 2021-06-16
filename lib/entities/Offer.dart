507435248293ea22e313139c4ee2f89364a42351
Lucero Gejaño <anlugeri2001@gmail.com>
2021-06-16 02:11:58 -0500

    merged with feature/offers

---
+++ lib/entities/Offer.dart
@@ -0,0 +1,17 @@
+class Offer {
+  String laundryService;
+  String discount;
+  String initialDate;
+  String endingDate;
+  String imglink;
+
+  Offer(
+      {
+        required this.laundryService,
+        required this.discount,
+        required this.initialDate,
+        required this.endingDate,
+        required this.imglink
+
+      });
+}
